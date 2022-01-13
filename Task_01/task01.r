b35750453f7a0cc0334a5b9ec3411e577fd3f227
brittdye24 <bd0040@mix.wvu.edu>
2022-01-12 19:02:21 -0500

    <Brittany Dye> Task 01 upload b

--- Task_01/task01.r
+++ Task_01/task01.r
@@ -1,2 +1,30 @@
 install.packages("swirl")
 library(swirl)
+library(swirl)
+swirl()
+1
+1
+1
+5+7
+x <- 5+7
+x
+y <- x-3
+y
+z <- c(1.1, 9, 3.14)
+?c
+z
+c(z, 555, z)
+
+my_sqrt <- sqrt(z-1)
+1
+my_sqrt <- sqrt(z-1)
+my_sqrt
+my_div <- z/my_sqrt
+1
+2
+3
+1
+my_div
+c(1, 2, 3, 4) + c(0, 10)
+c(1, 2, 3, 4) + c(0, 10, 100)
+1
