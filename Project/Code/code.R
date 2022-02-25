178cb7714a3e7d60627148771575d388764c0fb5
brittdye24 <bd0040@mix.wvu.edu>
2022-02-24 20:31:20 -0500

    Project

---
+++ Project/Code/code.R
@@ -0,0 +1,5 @@
+setwd('C:\\Users\\britt\\OneDrive\\Hypothesis')
+MIAMI01 <- read.csv("C:/Users/britt/OneDrive/Hypothesis/MIAMI01.csv")
+HAWAII01 <- read.csv("C:/Users/britt/OneDrive/Hypothesis/HAWAII01.csv")
+MIAMI01
+HAWAII01
---
+++ Project/Text/text.R
@@ -0,0 +1,2 @@
+#The data will test the hypothesis of whether the weight and longevity of Monarch butterflies coevolve
+  #over 11 lineages in two different locations. 
